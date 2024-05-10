abce02d85f60b409e447ddca9846f23af724768e
dibyenduaditya <153230157+dibyenduaditya@users.noreply.github.com>
2024-05-10 13:30:11 +0530

    Update XeonCheems13.js

--- XeonCheems13.js
+++ XeonCheems13.js
@@ -1,10 +1,10 @@
-//base by DGXeon
+//base by [ !! < 𝐌ʀ.x-𝐀ᴅɪᴛʏᴀ > !! ]
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @DGXeon
-//Instagram: unicorn_xeon13
-//Telegram: t.me/xeonbotinc
-//GitHub: @DGXeon
-//WhatsApp: +916909137213
+//YouTube: @Aditya Editx
+//Instagram: i_aam_adi07
+//Telegram: t.me/7318812338
+//GitHub: @dibyenduaditya
+//WhatsApp: +918653805156
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 require('./lib/listmenu')
 const {
